e6b0d748e75dcdd4569657c7c5467ad8e87bf482
ridmapabasara <92ridmapabasara@gmail.com>
2019-01-09 23:16:19 +0530

    ssl

--- install-rabbitMQ/files/rabbitMQdockerfiles/init.sh
+++ install-rabbitMQ/files/rabbitMQdockerfiles/init.sh
@@ -6,10 +6,12 @@ rabbitmqctl add_vhost sensu
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
 rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
 rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
+chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie;\
 
 echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
 echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
 #------------------------------------------------
+
 
 # $@ is used to pass arguments to the rabbitmq-server command.
 # For example if you use it like this: docker run -d rabbitmq arg1 arg2,
